92a5d67e956ae367cfa9a694b1e157847a1c205c
GabrielNMV <gabrielmuva111@gmail.com>
2024-10-09 17:41:37 -0300

    Create valorant.py
    aaa

---
+++ valorant.py
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi primer pagina 1</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>VALORANT</h1>
+    <p>Valorant es un juego de disparos táctico 5v5 basado en personajes, cada personaje tiene 4 habilidades, actualmente existen 19 armas: una de cuerpo a cuerpo, unos subfusiles, unos fusiles, unos francotiradores, unas escopetas y unas ametralladoras ligeras, hay 10 mapas algunos con mecanicas nuevas como portales y puertas que se abren y cierran. Existen torneos de este juego que puedes ganar trofeos, ejemplos: Champions y Americas</p>
+    <p>Combina tu estilo y experiencia en un escenario global y competitivo. Tienes 13 rondas para atacar y defender tu lado con armas precisas y habilidades tácticas. Además, al contar con una sola vida por ronda, tendrás que pensar más rápido que tu oponente si quieres sobrevivir. Enfréntate a enemigos en los modos competitivo y normal, así como en Deathmatch y Spike Rush.</p>
+    <img src="https://staticg.sportskeeda.com/editor/2022/08/8fd46-16594963257131-1920.jpg" alt="Imagen">
+
+    <h2>Personajes</h2>
+
+    <ul>
+    <li>Existen actualmente 25 personajes, todos tienen un rol seleccionado; controlador, iniciado, duelista y controlador, cada uno tiene diferentes habilidades que ayudan al equipo.</li>
+    </ul>
+    <h2>Estrategias</h2>
+    <li>Existen muchas estrategias pero algunas serian: No asomar, ahorrar, defender, ir con todo.
+        
+    </li>
+    <img src="https://cdn1.epicgames.com/offer/cbd5b3d310a54b12bf3fe8c41994174f/EGS_VALORANT_RiotGames_S1_2560x1440-91dc9490f14942ad5eeef278eb3ef4a6" alt="Imagen">
+</body>
+</html>
+
+
+body {
+    font-family: Arial, Verdana, sans-serif; /* Familia de fuentes */
+    font-size: 11pt; /* El tamaño de la fuente principal en el tag <p></p> */
+    background-color: #000000; /* El color del fondo de la página web */
+    color: #ffffff; /* El color del texto */
+    }
+    h1 {
+    color: #f70f0f; /* El color de la cabecera */
+    font-size: 24pt; /* El tamaño de la fuente de la cabecera/
+    font-family: Georgia, Times, serif; /* La familia tipográfica utilizada en la cabecera */
+    }
+    h2{
+    color:#f70f0f;
+    border-color: #b01005;
+    }
